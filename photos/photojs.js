6bd423aef0233f859fc8db4161246281753db6f6
GBelonozhko <44050866+GBelonozhko@users.noreply.github.com>
2019-03-16 10:52:32 -0700

    Collection of practice projects

---
+++ photos/photojs.js
@@ -0,0 +1,11 @@
+
+var h1 = document.querySelector("h1");
+
+h1.style.color = "pink";
+h1.style.border = "10px solid red";
+
+let pics = document.querySelector("img");
+
+pics.addEventListener("hover",function(){
+    pics.
+});
